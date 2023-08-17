ad9dc06239c57ddc71285be42cf271da9a154e6b
Arko Kundu <109503991+arkokundu500@users.noreply.github.com>
2023-08-17 10:06:09 +0530

    Added project files and photos - 1

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Content Creator", "College Student","Competitive Coder","Cloud Architect"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+function scrollToTop(){
+  window.scrollTo(0, 0);
+}
