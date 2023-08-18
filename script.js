0a945d28292abfae5f3218e5550bc027d8ed0073
Arko Kundu <109503991+arkokundu500@users.noreply.github.com>
2023-08-18 20:08:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "Content Creator", "College Student","Competitive Coder","Cloud Architect"],
+  strings: ["Web Developer", "Content Creator", "College Student","Competitive Coder","Cloud Architect"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
@@ -8,3 +8,15 @@ var typed = new Typed(".multiple-text", {
 function scrollToTop(){
   window.scrollTo(0, 0);
 }
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab");
+        }
